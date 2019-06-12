6f8456e355f9e680d5e86519b8d96869d8ed3fe7
Kasem Saeed <kasem.jaffer@gmail.com>
2019-06-12 13:28:35 -0700

    Add comment describing the method

--- lib/flutter_absolute_path.dart
+++ lib/flutter_absolute_path.dart
@@ -6,6 +6,8 @@ class FlutterAbsolutePath {
   static const MethodChannel _channel =
       const MethodChannel('flutter_absolute_path');
 
+  /// Gets absolute path of the file from android URI or iOS PHAsset identifier
+  /// The return of this method can be used directly with flutter [File] class
   static Future<String> getAbsolutePath(String uri) async {
     final Map<String, dynamic> params = <String, dynamic>{
       'uri': uri,
