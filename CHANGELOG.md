9bd92b70e215e95bd1fdb99fef869e00d987cd09
Kasem Saeed <kasem.jaffer@gmail.com>
2019-06-24 18:18:29 -0700

    Fix iOS uri that starts with "file://"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.5
+
+* Fix iOS uri that starts with "file://"
+
 ## 1.0.4
 
 * Remove un-necessary jar libraries
--- ios/Classes/SwiftFlutterAbsolutePathPlugin.swift
+++ ios/Classes/SwiftFlutterAbsolutePathPlugin.swift
@@ -3,30 +3,34 @@ import UIKit
 import Photos
 
 public class SwiftFlutterAbsolutePathPlugin: NSObject, FlutterPlugin {
-
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_absolute_path", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterAbsolutePathPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-   if(call.method == "getAbsolutePath"){
-      guard let uri = (call.arguments as? Dictionary<String, String>)?["uri"] else {
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_absolute_path", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterAbsolutePathPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if(call.method == "getAbsolutePath"){
+            guard let uri = (call.arguments as? Dictionary<String, String>)?["uri"] else {
                 result(FlutterError(code: "assertion_error", message: "uri is required.", details: nil))
                 return
             }
-    let phAsset = PHAsset.fetchAssets(withLocalIdentifiers: [uri], options: .none).firstObject
-    if(phAsset == nil) {
-        result(nil)
-        return
+            if (uri.starts(with: "file://")) {
+                result( uri.replacingOccurrences(of: "file://", with: ""))
+                return
+            }
+            let phAsset = PHAsset.fetchAssets(withLocalIdentifiers: [uri], options: .none).firstObject
+            if(phAsset == nil) {
+                result(nil)
+                return
+            }
+            let editingOptions = PHContentEditingInputRequestOptions()
+            editingOptions.isNetworkAccessAllowed = true
+            phAsset!.requestContentEditingInput(with: editingOptions) { (input, _) in
+                let url = input?.fullSizeImageURL?.absoluteString.replacingOccurrences(of: "file://", with: "")
+                result(url)
+            }
+        }
     }
-    let editingOptions = PHContentEditingInputRequestOptions()
-    editingOptions.isNetworkAccessAllowed = true
-    phAsset!.requestContentEditingInput(with: editingOptions) { (input, _) in
-        let url = input?.fullSizeImageURL?.absoluteString.replacingOccurrences(of: "file://", with: "")
-        result(url)
-      }
-    }
-  }
 }
